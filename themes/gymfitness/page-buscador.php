f7ab44802c852a3f728701e13cdab423ffe22e3c
Luis Bernal <blacksk81@gmail.com>
2020-02-01 10:34:04 +0100

    Realmente no es un buscador
    Es como usar un json externo y mostrarlo en pantalla

--- themes/gymfitness/page-buscador.php
+++ themes/gymfitness/page-buscador.php
@@ -1,15 +1,28 @@
 <?php get_header(); // Template Name: Buscador ?>
-	<div class="contenedor pagina seccion con-sidebar">
+	<div class="contenedor pagina seccion ">
 		<div class="contenido-principal">
-
-			<h1>Buscador</h1>
-
+			<p>Buscador Avanzado...</p>
+			<?php //buscado input 1208321oebedjlwq ?>
 			<div class="buscador">
-				<input type="text" name="buscar" id="buscar" placeholder="Buscar Receta">
-				
+				<input type="text" name="buscar" id="buscar" placeholder="Buscar recetas">
+				<button id="btn_buscar" type="button" class="button">Buscar</button>
 			</div>
+			<h1>soy el json weyyy</h1>
+		
+			<?php 
+				$jsondata = file_get_contents("https://jsonplaceholder.typicode.com/users/1");
+				$obj  = json_decode( $jsondata , false );
+				echo "<pre>";
+				print_r($obj);
+				echo "</pre>";
+				echo "<hr>";
+				print_r($obj->address->street);
+			?>
+
+
 
 		</div>
-		<?php // get_sidebar('home'); ?>
 	</div>
+
 <?php get_footer(); ?>
+
